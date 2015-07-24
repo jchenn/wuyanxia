1ae1ebe4f3021c51d81f66b4740a32615a6cae73
wuww5511 <wuww5511@126.com>
2015-07-24 23:01:02 +0800

    wuww5

--- app/js/notice/notice.js
+++ app/js/notice/notice.js
@@ -1,4 +1,4 @@
-angular.module('notice',[])
+angular.module('notice',['ionic'])
     .config(function($stateProvider){
     $stateProvider
      //邀请回复界面 
@@ -32,43 +32,45 @@ angular.module('notice',[])
     })
    
     .controller('c1',function($scope,$http, $location){
-    $scope.imgSrc="a.jpg";
-    $scope.notices=[
-        {
-            active:1,
-            id:1,
-            type:0,
-            title:"邀请回复",
-            content:"木鱼拒绝了您的合租请求",
-            time:"13:14 PM"
-        },
-        {
-            active:0,
-            id:1,
-            type:1,
-            title:"室友私信",
-            content:"木鱼说很高兴认识你",
-            time:"13:14 PM"
-        }
-    ];
-    $scope.locate=function(type){
-        //0 邀请回复
-        if(type==0){
-           return "#/notice/reply";
-        }
-        //1 聊天
-        else if(type==1){
-            return "#/notice/chart";
-        }
-    };
-    $scope.delete=function(id){
-        alert(id);
-    };
+        $scope.imgSrc="a.jpg";
+        $scope.notices=[
+            {
+                active:1,
+                id:1,
+                type:0,
+                title:"邀请回复",
+                content:"木鱼拒绝了您的合租请求",
+                time:"13:14 PM"
+            },
+            {
+                active:0,
+                id:1,
+                type:1,
+                title:"室友私信",
+                content:"木鱼说很高兴认识你",
+                time:"13:14 PM"
+            }
+        ];
+        $scope.locate=function(type){
+            //0 邀请回复
+            if(type==0){
+               return "#/notice/reply";
+            }
+            //1 聊天
+            else if(type==1){
+                return "#/notice/chart";
+            }
+        };
+        $scope.delete=function(id){
+            alert(id);
+        };
 })
 .controller('chartCtrl',function($scope){
     $scope.title="木鱼";
+    $scope.back=function(){history.go(-1);};
 })
 .controller('replyCtrl',function($scope){
     $scope.title="邀请回复";
+    $scope.back=function(){history.go(-1);};
 })
 ;

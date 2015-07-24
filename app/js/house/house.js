2df36ec3cf7c5fec4466687ab4e49939db0465c8
Jarvis Chen <meniac@yeah.net>
2015-07-24 12:12:39 +0800

    添加 gulp watch 任务，在修改代码时自动部署

--- app/js/house/house.js
+++ app/js/house/house.js
@@ -1,16 +1,17 @@
 angular.module('house', ['house.ctrl', 'house.directive', 'house.service'])
 
-.constant('AreaList', ['滨江', '西湖', '江干'])
+.constant('AreaList', ['不限', '滨江', '西湖', '江干'])
 
 .constant('PriceList', [
-  {key: 1, value: '500以下'},
-  {key: 2, value: '500-1000'},
-  {key: 3, value: '1000-1500'},
-  {key: 4, value: '1500-2000'},
-  {key: 5, value: '2000-3000'},
-  {key: 6, value: '3000-5000'},
-  {key: 7, value: '5000-8000'},
-  {key: 8, value: '8000以上'}
+  {key: 1, value: '不限'},
+  {key: 2, value: '500以下'},
+  {key: 3, value: '500-1000'},
+  {key: 4, value: '1000-1500'},
+  {key: 5, value: '1500-2000'},
+  {key: 6, value: '2000-3000'},
+  {key: 7, value: '3000-5000'},
+  {key: 8, value: '5000-8000'},
+  {key: 9, value: '8000以上'}
 ])
 
 .constant('RoomList', [

b8e57a485eeb205c945d8a2724d1fbd1facb253c
lbl <lbl_181@163.com>
2015-07-29 10:33:36 +0800

    modify fatory

--- app/js/me/me-service.js
+++ app/js/me/me-service.js
@@ -3,6 +3,9 @@ angular.module('me.service', [])
 .factory('PersonalInfo',function(){
   return {
     'lookStatus' : '正在寻找',
+    'year': '1988',
+    'month': '10',
+    'day': '22',
     'birth': '1988-10-22',
     'name': '',
     'sex': '',

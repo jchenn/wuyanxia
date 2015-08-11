fcaa581eba1faff7ff2d2682b757646b7ec05133
lbl <lbl_181@163.com>
2015-08-11 15:36:36 +0800

    精简代码

--- app/js/auth/auth-ctrl.js
+++ app/js/auth/auth-ctrl.js
@@ -170,7 +170,7 @@ angular.module('auth.ctrl', ['ionic'])
                         //更新PersonalInfo
                         PersonalInfoMange.clear();
                         PersonalInfoMange.update({
-                            name: $scope.formData.nickname
+                            nickName: $scope.formData.nickname
                         });
                         PersonalInfoMange.update(resp.data);
 
--- app/js/me/me-ctrl.js
+++ app/js/me/me-ctrl.js
@@ -5,43 +5,41 @@ angular.module('me.ctrl', [])
 
         $scope.data = {};
         angular.extend($scope.data,PersonalInfo);
-        console.log($scope.data);
 
-
+        //调用摄像头
         $scope.showCamera = function(){
             TakePhoto.showCamera(0,function(imageData){
                 $scope.data.avatar = imageData;
-                console.log(PersonalInfo);
-                console.log($scope.data);
-                //alert(imageData);
             });
-            /*PersonalInfoMange.update({'avatar':url});
-            $scope.data.avatar = url;*/
         };
 
+        //加载性别选择模板
         $ionicModal.fromTemplateUrl('templates/me/sex-modal.html', {
             scope: $scope,
             animation: 'slide-in-up'
         }).then(function(modal){
             $scope.modal = modal;
         });
+
         //显示modal
         $scope.openSex = function(){
             $scope.modal.show();
         };
+
         //隐藏modal
         $scope.closeSex = function(){
             $scope.modal.hide();
         };
+
         //选择性别
         $scope.selectSex = function(sex){
             $scope.data.gender = sex;
             $scope.closeSex();
         };
 
+        //显示日期选择框
         $scope.showDate = function(){
             dateSelect.showDate($scope);
-            console.log(PersonalInfo);
         };
 
         //跳过按钮
@@ -53,18 +51,23 @@ angular.module('me.ctrl', [])
             }
         };
 
-
         //提交数据
         $scope.finishRegister = function(){
+
             //检查数据
-
-
-           /* if(Check.getLen($scope.data.avatar) < 1 || $scope.data.avatar == "http://223.252.223.13/Roommates/photo/photo_default.jpg"){
+            if(Check.getLen($scope.data.avatar) < 1 || $scope.data.avatar == "http://223.252.223.13/Roommates/photo/photo_default.jpg"){
                 $ionicPopup.alert({
                     template: '请上传头像'
                 });
                 return false;
-            }*/
+            }
+
+            if(Check.getLen($scope.data.nickName) < 1 || Check.getLen($scope.data.nickName) > 30){
+                $ionicPopup.alert({
+                    template:"昵称不能为空或者大于15个汉字"
+                });
+                return false;
+            }
 
             if(Check.getLen($scope.data.gender) < 1){
                 $ionicPopup.alert({
@@ -72,24 +75,28 @@ angular.module('me.ctrl', [])
                 });
                 return false;
             }
+
             if(Check.getLen($scope.data.birthday) < 1){
                 $ionicPopup.alert({
                     template:"请选择出生日期"
                 });
                 return false;
             }
+
             if(Check.getLen($scope.data.company) < 1 || Check.getLen($scope.data.company) > 30){
                 $ionicPopup.alert({
                     template: "公司名称不能为空或过长"
                 });
                 return false;
             }
+
             if(Check.getLen($scope.data.job) < 1 || Check.getLen($scope.data.job) > 30){
                 $ionicPopup.alert({
                     template: "岗位名称不能为空或过长"
                 });
                 return false;
             }
+
             if(!Check.checkPrice($scope.data.phone)  || Check.getLen($scope.data.phone) != 11){
                 $ionicPopup.alert({
                     template: "电话必须为11位的数字"
@@ -97,6 +104,7 @@ angular.module('me.ctrl', [])
                 return false;
             }
 
+            //组装用户填写数据为对象
             var sendData = {
                 'nickName' : $scope.data.nickName,
                 'gender' : $scope.data.gender,
@@ -106,7 +114,7 @@ angular.module('me.ctrl', [])
                 'phone' : $scope.data.phone
             };
 
-            //先上传头像
+            //先上传头像，然后把照片存到本地缓存
             TakePhoto.uploadPic($scope.data.avatar);
             PersonalInfoMange.update({'avatar' : $scope.data.avatar});
 
@@ -118,51 +126,51 @@ angular.module('me.ctrl', [])
                 timeout: 2000
             });
 
-            res.success(function(response, status, headers, config){
-                console.log(response);
+            res.success(function(response){
+
                 if(response.errno == 0){
                     PersonalInfoMange.update($scope.data);
                     PersonalInfoMange.update({'completeInfo' : true});
+
                     if(!PersonalInfoMange.get('tags')){
                         $scope.go('/quiz');
                     }else{
                         $scope.go('/menu/people-list');
                     }
+
                 }else if(response.errno == 1){
                     $ionicPopup.alert({
                         template: response
                     });
                 }
-            }).error(function(response, status, headers, config){
-                console.log(response);
+            }).error(function(response){
+                $ionicPopup.alert({
+                    template: response
+                });
             });
         }
-    }).controller('InfoShow', function($scope, $ionicActionSheet,$ionicPopup, $ionicModal,dateSelect,TakePhoto, $ionicPopover, $timeout, PersonalInfo ,$http, PersonalInfoMange, DayInit){
-        console.log(1111);
-        console.log(PersonalInfo);
-        angular.extend(PersonalInfo, DayInit);
-        $scope.data = PersonalInfo;
-        console.log(PersonalInfo);
+    }).controller('InfoShow', function($scope, $ionicActionSheet,$ionicPopup, $ionicModal,dateSelect,TakePhoto, $ionicPopover, $timeout, PersonalInfo ,$http, PersonalInfoMange){
 
+        $scope.data = PersonalInfo;
+
+        //调用摄像头
         $scope.showCamera = function(){
-            console.log(PersonalInfo);
-            console.log('wo shi pai zhao zhiqian');
+
             TakePhoto.showCamera(1,function(imageData){
                 $scope.data.avatar = imageData;
             });
-            console.log('wo shi pai zhao zhihou ');
-            console.log(PersonalInfo);
-            console.log($scope.data);
+
         };
 
+        //显示日期选择框
         $scope.showDate = function(){
+
             dateSelect.showDate($scope , $http,1);
 
         };
-        console.log($scope.data);
-
 
         $scope.showStatus = function(){
+
             var hideSheet = $ionicActionSheet.show({
                 buttons: [
                     {text: '正在寻找'},
@@ -173,9 +181,10 @@ angular.module('me.ctrl', [])
                     hideSheet();
                 },
                 buttonClicked: function(index){
+
+                    //缓存原本的状态，如果所选的状态跟原本的相同，就不发送请求
                     var temp = PersonalInfoMange.get('lookStatus');
                     $scope.data.lookStatus = index == 0 ? 0 : 1;
-
 
                     if(temp != $scope.data.lookStatus){
                         var obj = {
@@ -187,14 +196,22 @@ angular.module('me.ctrl', [])
                             data: obj,
                             timeout: 2000
                         });
-                        res.success(function(response, status, headers, config){
+                        res.success(function(response){
+
                             if(response.errno == 0){
                                 PersonalInfoMange.update({'lookStatus': $scope.data.lookStatus});
                             }else if(response.errno == 1){
-                                console.log(response.errno);
+                                $ionicPopup.alert({
+                                    template: response.errno
+                                });
                             }
-                        }).error(function(response, status, headers, config){
-                            console.log(response);
+
+                        }).error(function(response){
+
+                            $ionicPopup.alert({
+                                template: response
+                            });
+
                         });
                     }
 
@@ -203,6 +220,7 @@ angular.module('me.ctrl', [])
             });
         };
 
+        //修改页面带上值，保存值
         $scope.modify = function(obj){
             PersonalInfo.title = obj.title;
             PersonalInfo.val = obj.val;
@@ -211,21 +229,23 @@ angular.module('me.ctrl', [])
         };
 
         //与注册信息页面重复的代码 start
-
         $ionicModal.fromTemplateUrl('templates/me/sex-modal.html', {
             scope: $scope,
             animation: 'slide-in-up'
         }).then(function(modal){
             $scope.modal = modal;
         });
+
         //显示modal
         $scope.openSex = function(){
             $scope.modal.show();
         };
+
         //隐藏modal
         $scope.closeSex = function(){
             $scope.modal.hide();
         };
+
         //选择性别
         $scope.selectSex = function(sex){
             var temp = $scope.data.gender;
@@ -253,13 +273,16 @@ angular.module('me.ctrl', [])
                             template: response
                         });
                     }
-                }).error(function(response, status, headers, config){
-                    console.log(response);
+                }).error(function(response){
+                    $ionicPopup.alert({
+                        template: response
+                    });
                 });
             }
             $scope.closeSex();
         };
     })
+
     //修改个人信息控制器
     .controller('EditorInfo', function($scope, $http,$ionicPopup, PersonalInfo, PersonalInfoMange, Check){
         $scope.data = PersonalInfo;
@@ -282,6 +305,7 @@ angular.module('me.ctrl', [])
                     return false;
                 }
             }
+
             //给服务器发请求
             var res = $http({
                 method: 'post',
@@ -289,7 +313,8 @@ angular.module('me.ctrl', [])
                 data: obj,
                 timeout: 2000
             });
-            res.success(function(response, status, headers, config){
+            res.success(function(response){
+
                 if(response.errno == 0){
                     if(response.finishInfo == 1){
                         PersonalInfoMange.update({'completeInfo' : true});
@@ -302,12 +327,15 @@ angular.module('me.ctrl', [])
                             template: response.message
                         });
                 }
-            }).error(function(response, status, headers, config){
+
+            }).error(function(response){
+
                 $ionicPopup.alert({
                     template: response.message
                 });
+
             });
-        }
+        };
 
         $scope.clearValue = function(){
             document.getElementById('textField').value = "";
